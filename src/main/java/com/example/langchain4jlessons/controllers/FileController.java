ed43d3389afd62ae2a00bb3580ba08273eab8c11
luissavighago <55593826+luissavighago@users.noreply.github.com>
2024-03-21 21:48:25 -0300

    #endpoint /file

--- src/main/java/com/example/langchain4jlessons/controllers/FileController.java
+++ src/main/java/com/example/langchain4jlessons/controllers/FileController.java
@@ -4,18 +4,15 @@ import com.example.langchain4jlessons.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RequestMapping("/files")
 @RestController
 public class FileController {
 
     @Autowired
     FileService fileService;
 
-    @PostMapping("/store")
+    @PostMapping("/file")
     public ResponseEntity<Object> storeFile() {
         return fileService.storeFile();
     }
